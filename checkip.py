b832d5aa1b3460d6729d50212d90aaf9502436f5
Tran Van Quoc <56865996+ntyinsec@users.noreply.github.com>
2023-07-26 18:55:12 +0700

    Update checkip.py

--- checkip.py
+++ checkip.py
@@ -6,12 +6,12 @@ user = os.environ["USER"]
 password = os.environ["PASSWORD"]
 
 iosv_l2_s1 = {
-    "device_type": "cisco_ios",
-    "ip": "192.168.1.233",
+    "device_type": "juniper",
+    "ip": "172.25.240.245",
     "username": user,
     "password": password,
 }
 
 net_connect = ConnectHandler(**iosv_l2_s1)
-output = net_connect.send_command("show ip int brief")
+output = net_connect.send_command("show configuration | display set")
 print(output)
